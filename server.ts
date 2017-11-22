6104ae864b10e2a5d80c95ea836b596d0559c1f8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-11-22 12:27:17 +0000

    Working with --bundle-dependencies=all off, or with --prod off, but not with both on

--- server.ts
+++ server.ts
@@ -1,14 +1,6 @@
-import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
-import { renderModuleFactory } from '@angular/platform-server';
-import { enableProdMode } from '@angular/core';
-
 import * as express from 'express';
 import { join } from 'path';
 import { readFileSync } from 'fs';
-
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
 
 // Express server
 const app = express();
@@ -20,36 +12,41 @@ const DIST_FOLDER = join(process.cwd(), 'dist');
 const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
+const {
+  AppServerModule,
+  AppServerModuleNgFactory,
+  LAZY_MODULE_MAP,
+  provideModuleMap,
+  renderModule,
+  renderModuleFactory,
+  enableProdMode
+} = require('./dist/server/main.bundle');
 
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+enableProdMode();
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
-
-/* - Example Express Rest API endpoints -
-  app.get('/api/**', (req, res) => { });
-*/
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
-// ALl regular routes use the Universal engine
+// All regular routes use Angular SSR
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  const renderOptions = {
+    document: template,
+    url: req.url,
+    extraProviders: [provideModuleMap(LAZY_MODULE_MAP)]
+  };
+
+  const renderPromise = AppServerModuleNgFactory
+    ? /* prod mode */ renderModuleFactory(AppServerModuleNgFactory, renderOptions)
+    : /* dev mode */ renderModule(AppServerModule, renderOptions);
+
+  renderPromise
+    .then(html => res.send(html), err => res.send(err))
+    .then(() => res.end());
 });
 
 // Start up the Node server
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,1 +1,9 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+
+// Because we want to use --bundle-dependencies=all, we export the SSR
+// APIs we want to invoke so they can be called from outside this bundle
 export { AppServerModule } from './app/app.server.module';
+export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+export { renderModule, renderModuleFactory } from '@angular/platform-server';
+export { enableProdMode } from '@angular/core';
